b709149e919db06169e1f833b6d41c2c3bb1beae
muhammadilham730 <mi8190044@gmail.com>
2024-08-29 11:51:23 +0700

    10print r array

---
+++ 10print r array.php
@@ -0,0 +1,21 @@
+<?php
+$array = array(
+    'name' => 'ilham',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'ilham',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+
+print "<p>";
+echo ($array);
