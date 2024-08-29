21b6242bbc7aed47b7cbd9dd33ed4fb00b71532f
muhammadilham730 <mi8190044@gmail.com>
2024-08-29 11:14:46 +0700

    04 menggunakan var_dump

---
+++ 04menggunakan var_dump0.php
@@ -0,0 +1,9 @@
+<?php
+$variabel_string = "hello ilham<p>";
+$variabel_integer = 139;
+$variabel_array = array(1, 2, "tiga", 4,5);
+var_dump($variabel_string);
+var_dump($variabel_integer);
+var_dump($variabel_array);
+echo"<br><br><br><strong>by si bray ilham";
+?>
