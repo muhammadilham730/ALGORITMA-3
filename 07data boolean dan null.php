f0e17168e0017ecfe7212df1795c1c61da6e7aca
muhammadilham730 <mi8190044@gmail.com>
2024-08-29 11:41:49 +0700

    07 data boolean dan null

---
+++ 07data boolean dan null.php
@@ -0,0 +1,12 @@
+<?php
+$var_boolean1 = true;
+$var_boolean2 = false;
+$var_null = null;
+
+var_dump($var_boolean1);
+print "<p>";
+var_dump($var_boolean2);
+print "<p>";
+var_dump($var_null);
+echo"<br><br><br><strong>by si bray ilhammm";
+?>
