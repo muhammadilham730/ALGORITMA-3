b6aef68e72aa2049058d9439956098440cd4b3dd
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 09:26:06 +0700

    24 strtouper dan strtolower

---
+++ 24 strtouper dan strtolower.php
@@ -0,0 +1,6 @@
+
+<?php
+echo strtoupper("Hello World!"); // Outputs: "HELLO WORLD!"
+echo strtolower("Hello World!"); // Outputs: "hello world!"
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
