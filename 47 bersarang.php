18960669aa7dd2f6efd71cfafd963466a28a7dc6
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:20:04 +0700

    47 bersarang

---
+++ 47 bersarang.php
@@ -0,0 +1,44 @@
+
+<?php
+$umur = 17;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+
+$umur = 27;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "<br>Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+
+$umur = 45;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "<br>Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
+
+?>
