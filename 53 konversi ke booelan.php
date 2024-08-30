4bed47c45ff274d9f76b633a6cc326cba39343f4
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:32:39 +0700

    53 konversi ke boolean

---
+++ 53 konversi ke booelan.php
@@ -0,0 +1,12 @@
+
+<?php
+$nilai = 0;
+
+if ($nilai) {
+    echo "Variabel ini bernilai true";
+} else {
+    echo "Variabel ini bernilai false";
+}
+// Keluaran akan "Variabel ini bernilai false" karena nilai 0 dianggap false.
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
