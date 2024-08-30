68b6d571234ddda29464ab150e6168683e7ffb33
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:48:35 +0700

    57 kombinasi dengan elseif

---
+++ 57 komnbinasi dengan  elseif.php
@@ -0,0 +1,13 @@
+
+<?php
+$suhu = 30;
+
+if ($suhu < 20) {
+    echo "Cuaca dingin.";
+} elseif ($suhu <= 30) {
+    echo "Cuaca hangat.";
+} else {
+    echo "Cuaca panas.";
+}
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
