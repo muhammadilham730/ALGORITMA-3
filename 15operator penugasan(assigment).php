98b5e62712752caad5cf3852b1b7f40d505889bb
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 09:01:21 +0700

    15operator penugasan(assigment)

---
+++ 15operator penugasan(assigment).php
@@ -0,0 +1,16 @@
+<?php
+$speed = 83;
+
+// ini opertor aritmatika
+$speed = $speed + 10;
+
+// maka nilai speed akan samadengan 83 + 10 = 93
+
+// ini operator penugasan
+$speed += 10;
+
+// sekarang nilai speed akan menjadi 93 + 10 = 103
+
+echo "hasil speed = $speed";
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
