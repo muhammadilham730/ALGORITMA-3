58b25ae611e397e8221e53369dd6624de3bd35e1
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 09:17:50 +0700

    20operator ternary

---
+++ 20operator ternary.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
+
+print_r("<p><strong><i>By Muhammad ilham </strong>");
