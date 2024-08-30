66307824c60d560f566ff0c3c7e3440821393764
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:26:28 +0700

    50 pernyataan elseif

---
+++ 50 pernyataan elseif.php
@@ -0,0 +1,11 @@
+
+<?php
+$nilai = 80;
+if ($nilai >= 90) {
+  echo "Nilai kamu A";
+} elseif ($nilai >= 80) {
+  echo "Nilai kamu B";
+} else {
+  echo "Nilai kamu C";
+}
+print_r("<p><strong><i>By Muhammad ilham</strong>");
