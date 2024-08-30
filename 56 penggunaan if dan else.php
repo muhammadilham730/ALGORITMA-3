59d089d8740b08f3512971b2d8641c469411c953
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:40:33 +0700

    56 penggunaan if dan else

---
+++ 56 penggunaan if dan else.php
@@ -0,0 +1,11 @@
+
+<?php
+$umur = 16;
+
+if ($umur >= 17) {
+    echo "Kamu sudah cukup umur.";
+} else {
+    echo "Kamu belum cukup umur.";
+}
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
