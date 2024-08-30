3723c8de4bc9dec7181d72e35b20863b164c618f
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:30:59 +0700

    52 boolean dalam pengambilan keputusan

---
+++ 52 boolean dalam pengambilan keputusan.php
@@ -0,0 +1,11 @@
+
+<?php
+$login_sukses = true;
+
+if ($login_sukses) {
+    echo "Selamat datang pengguna!";
+} else {
+    echo "Login gagal, silakan coba lagi.";
+}
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
