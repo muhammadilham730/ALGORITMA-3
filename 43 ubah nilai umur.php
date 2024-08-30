169968d86669ec181338e5791f585099dc348d24
muhammadilham730 <mi8190044@gmail.com>
2024-08-30 10:10:11 +0700

    43 ubah nilai umur

---
+++ 43 ubah nilai umur.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$umur = 19;
+
+if ($umur < 18 ){
+    echo "Kamu tidak boleh membuka situs ini!";
+} else {
+    echo "Selamat datang di website kami!";
+}
+
+print_r("<p><strong><i>By Muhammad ilham</strong>");
+
+?>
